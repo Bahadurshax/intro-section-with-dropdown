57eba319c3a0a7a5c2c6e39d6f28d9f083269966
Bahadur <arzimbetov.bahadir@inbox.ru>
2023-08-10 18:08:14 +0500

    Improve my solution

--- script.js
+++ script.js
@@ -1,24 +1,22 @@
-// main nav dropdown menu toggle btn
+function toggleEl(el) {
+    const isOpen = el.getAttribute('aria-expanded') === 'true';
+
+    const attr = isOpen ? false : true;
+    el.setAttribute('aria-expanded', attr);
+}
+
+
 const dropdownBtns = document.querySelectorAll('.dropdown-toggle');
 
 dropdownBtns.forEach(btn => {
-    btn.addEventListener('click', event => {
-        event.preventDefault();
-        event.currentTarget.parentElement.classList.toggle('open');
+    btn.addEventListener('click', () => {
+        toggleEl(btn.parentElement);
     })
 })
 
-// mobile burger btn
-const mobileBtn = document.querySelector('.mobile-btn');
-const mobileBtnImg = document.querySelector('.mobile-btn img');
-const mobileMenu = document.querySelector('.navbar-collapse');
+const mobileBtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.navbar-collapse');
 
-mobileBtn.addEventListener('click', event => {
-    mobileMenu.classList.toggle('show');
-
-    if (mobileMenu.classList.contains('show')) {
-        mobileBtnImg.src = 'images/icon-close-menu.svg';
-    } else {
-        mobileBtnImg.src = 'images/icon-menu.svg';
-    }
+mobileBtn.addEventListener('click', () => {
+    toggleEl(menu);
 })
